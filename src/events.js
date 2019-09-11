5a6367f24a9ccd4ddc4d66443645619bdc1168fc
Andy Lee <andy5090@naver.com>
2019-09-11 17:02:41 +0900

    paint socket

--- src/events.js
+++ src/events.js
@@ -4,7 +4,13 @@ const events = {
   disconnect: "disconnect",
   disconnected: "disconnected",
   sendMsg: "sendMsg",
-  newMsg: "newMsg"
+  newMsg: "newMsg",
+  beginPath: "beginPath",
+  strokePath: "strokePath",
+  beganPath: "beganPath",
+  strokedPath: "strokedPath",
+  fill: "fill",
+  filled: "filled"
 };
 
 export default events;
